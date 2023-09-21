02f3e62a2a0f724630c899d571c01fe80a1c6999
Иван Хайрулин <ikhairulin@gmail.com>
2023-09-21 16:05:37 +0300

    fix errors and readme

--- src/test/java/com/example/test/MainTests.java
+++ src/test/java/com/example/test/MainTests.java
@@ -1,10 +1,10 @@
-package com.example.test;
+package com.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class MainTests {
 
 	@Test
 	void contextLoads() {
